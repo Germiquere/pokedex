406780a7e67319d0fd5534b141059e3531cb50b8
Sergio Aciar <aciarenator@gmail.com>
2023-01-05 17:32:42 -0300

    agregue el sonido a los pokemon

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const greenBoxUno = document.getElementById('greenBox1');
 const onOffButton = document.getElementById('onOffButton');
 const rightCross = document.getElementById('rightcross');
 const leftCross = document.getElementById('leftcross');
-const splash = document.getElementById('splash')
+const splash = document.getElementById('splash');
+const glassIndicator = document.getElementById('buttonGlass');
+
+const arrayAlmacenamiento = [];
+let posicionActual = 0;
+let crossActiva = false;
+
 // cargar primero el dom y despues ejecuta el async
 document.addEventListener('DOMContentLoaded',()=>{
     almacenamientoPokemon('http://pokeapi.co/api/v2/pokemon?limit=150'); 
@@ -13,9 +19,6 @@ document.addEventListener('DOMContentLoaded',()=>{
     nextPokemon();
     backPokemon();
 })
-
-const arrayAlmacenamiento = []
-let posicionActual= 0
 
 // datoss de pokemons
 const almacenamientoPokemon = async(url) =>{
@@ -37,24 +40,19 @@ const almacenamientoPokemon = async(url) =>{
 
     // }
 }
-// const reStartAnimation = (()=>{
-//     splash.style.animation = "";
-//  })
+
 // PRENDER POKEDEX
 const onPokedex =()=>{
     onOffButton.addEventListener('click',()=>{
         divPicture.classList.toggle('onoff');
         onOffButton.classList.toggle('onoff')
-        // console.log(arrayAlmacenamiento[0]);
-        // divPicture.innerHTML = `<img src="./assets/img/pokemon.png" alt="un pokemon" id="splash">`;
+        //se ejecuta el splash screen con el logo de pokemon
         splash.style.animation = "pokemon 3s ease";
-        // splash.classList.toggle('splash')
-        if(onOffButton.classList.contains('onoff')){
-
-            // splash.classList.add('splash')
-            setTimeout(printData,3000,arrayAlmacenamiento[0])
+        //si el boton de encendido esta en ON entonces muestra el primer pokemon y habilita el cross button
+        if(onOffButton.classList.contains('onoff')){            
+            setTimeout(printData,3000,arrayAlmacenamiento[0]);            
         }else{
-            
+            crossActiva =false;
             pictureBox.innerHTML = '';
             greenBoxUno.textContent ='';
             posicionActual = 0
@@ -63,18 +61,23 @@ const onPokedex =()=>{
         }
     })
 }
+
+//imprime la imagen en el cuadro 
 const printData = (data)=>{
     pictureBox.innerHTML = `<img src=${data.sprites.other.dream_world.front_default} alt="un pokemon">`;
     greenBoxUno.textContent = data.name.toUpperCase();
-
+    
+    glassIndicator.style.background = '-webkit-radial-gradient(var(--green-miniButton-glass-color-dos), var(--green-miniButton-glass-color-uno))' ;
+    crossActiva =true;
 }
-// NEXT
+// NEXT POKEMON
 const nextPokemon = ()=>{
     rightCross.addEventListener('click',()=>{
-        if(onOffButton.classList.contains('onoff')){
+        if(onOffButton.classList.contains('onoff') && crossActiva){
             if((arrayAlmacenamiento.length-1) > posicionActual){
                 printData(arrayAlmacenamiento[posicionActual+1]);
-                posicionActual++
+                speechName(greenBoxUno.innerHTML);
+                posicionActual++;
             }
         
        
@@ -82,51 +85,39 @@ const nextPokemon = ()=>{
         
     })
 }
-// BACK
+// BACK POKEMON
 const backPokemon = ()=>{
     leftCross.addEventListener('click',()=>{
-        if(onOffButton.classList.contains('onoff')){
+        if(onOffButton.classList.contains('onoff') && crossActiva){
             if(posicionActual> 0){
                 printData(arrayAlmacenamiento[posicionActual-1]);
+                speechName(greenBoxUno.innerHTML);
                 posicionActual--
             }
         }
         
     })
 }
-// console.log(arrayAlmacenamiento);
-// const search = () =>{
- 
-//     divPicture.innerHTML = `
-//     <section class="d-flex flex-column align-items-center justify-content-center d-none"  id="loading">
-//                 <strong class="mb-2">Cargando</strong>
-//                 <div class="spinner-border " role="status" aria-hidden="true"></div>
-//             </section>`;
-//     loadingDAta()
-//     // img.classList.remove('display')
-//     const textoIngresado = inputSearch.value.toLowerCase()
-//     if(textoIngresado.length>0 ){
 
-//         let elemento = []
-//         arrayAlmacenamiento.forEach(p=>{
-//             if(p.name.includes(textoIngresado)){
-//                 elemento = p  
-//             }
-//     })
-
-//     if(elemento.name === textoIngresado){
-//       setTimeout(()=>{
-//         loadingDAta()
-//         printData(elemento)
-//       },3000)
-//     }
-  
-// }
+//funcion para reproducir el nombre del pokemon por audio
+function speechName(name){
+    const utterance = new SpeechSynthesisUtterance(name);
+    utterance.rate = 1;
+    //esto es para que se pueda clickear el siguiente pokemon una vez que termine el audio
+    utterance.addEventListener('end',()=>{
+        crossActiva = true;
+    })
+    crossActiva = false;
+    utterance.voice = getVoice(2);
+    speechSynthesis.speak(utterance);
     
-// }
-// const loadingDAta = ()=>{
-//     const loading = document.getElementById('loading');
-//     loading.classList.toggle('d-none');
-// }
+}
 
-// inputSearch.addEventListener('keyup',search)
+//funcion que devuelve la voz seleccionada 0 y 1:mujer español, 2:hombre español
+function getVoice(voiceIdx){
+    const voices = window.speechSynthesis.getVoices();
+    if(voiceIdx >= 0 && voiceIdx <voices.length)
+        return voices[voiceIdx];
+    else
+        return voices[0];
+}
