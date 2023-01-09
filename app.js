2665563b7ce55e38a0bfe703aa3bfa2b9bb31317
Germiquere <german150703.miquere@gmail.com>
2023-01-09 17:09:32 -0300

    implementacion de speech 2

--- app.js
+++ app.js
@@ -7,17 +7,31 @@ const rightCross = document.getElementById('rightcross');
 const leftCross = document.getElementById('leftcross');
 const splash = document.getElementById('splash');
 const btnSearch = document.getElementById('btn');
-const errorMessage = document.getElementById('error-message')
-const btnCancel = document.getElementById('btn-cancel')
+const errorMessage = document.getElementById('error-message');
+const btnCancel = document.getElementById('btn-cancel');
+const btnStop = document.getElementById('btn-stop');
+const btnResume = document.getElementById('btn-resume');
 
 // const glassIndicator = document.getElementById('buttonGlass');
 const utterance = new SpeechSynthesisUtterance();
 utterance.voice = window.speechSynthesis.getVoices()[5];
 const arrayAlmacenamiento = [];
 const arraySpeciesPokemon = [];
-let typeEs = '';
-let typeGeneraEs = '';
-let TypeDetailEs = '';
+const typePokemonEn = [];
+let count = 0;
+
+const typePokemonEnPlusId = typePokemonEn.map(e=>{
+    const container = {};
+    container.id = count
+    container.names = e.names
+    count++
+    return container
+});
+
+
+// let typeEs = '';
+// let typeGeneraEs = '';
+// let TypeDetailEs = '';
 let posicionActual = 0;
 let crossActiva = false;
 let canSearch = false
@@ -46,6 +60,14 @@ const almacenamientoPokemon = async(url) =>{
             
         }
         
+        for (let index = 0; index < arrayAlmacenamiento.length; index++) {
+            const res = await fetch(arrayAlmacenamiento[index].types[0].type.url);
+            const data = await res.json();
+            typePokemonEn.push(data);
+            
+            
+        }
+      
         for (let i = 0; i < arrayAlmacenamiento.length; i++) {
             const res = await fetch(arrayAlmacenamiento[i].species.url);
             const species = await res.json();
@@ -57,11 +79,17 @@ const almacenamientoPokemon = async(url) =>{
     } catch (error) {
         console.log(error)
     }
-    // finally{
-
-    // }
 }
 
+
+// para obtener el id. hice un map del array y le agregue el id pero tengo problemas para llamarlo despues de que se haga el push al primer array porque creo que pasa antes de que termine el fetch
+
+const getIDPokemon = (pokemon)=>{
+    const id = typePokemonEnPlusId.find(e=>e.id === pokemon.id)
+    return id[0].names
+}
+console.log(typePokemonEnPlusId[0]);
+// let typePokemonEnPlusId = []
 // PRENDER POKEDEX
 const onPokedex =()=>{
     onOffButton.addEventListener('click',()=>{
@@ -69,16 +97,21 @@ const onPokedex =()=>{
         onOffButton.classList.toggle('onoff');
         btnSearch.classList.toggle('onoff');
         splash.style.animation = "pokemon 3s ease";
+        
+       
         if(onOffButton.classList.contains('onoff')){
-           console.log(getTypePokemon(arrayAlmacenamiento[0].types[0].type.url))
-
+      
+            // console.log(typePokemonEnPlusId[0]);
             setTimeout(()=>{
                 printData(arrayAlmacenamiento[0]);
-                getTypeGenera(arraySpeciesPokemon[0]);
-                getDetails(arraySpeciesPokemon[0])
-                speechName(generarTexto(arrayAlmacenamiento[0].name,typeEs,typeGeneraEs,TypeDetailEs));
+                // console.log(typePokemonEn);
+                
+                
+
+                // NO BORRAR---------------------------------------------------------------
+                speechName(generarTexto(arrayAlmacenamiento[0].name,getTypePokemonEs(typePokemonEn[0]),getTypeGenera(arraySpeciesPokemon[0]),getDetails(arraySpeciesPokemon[0])));
                 search()
-                btnSearch.disabled = false;
+                // btnSearch.disabled = false;
             },3000)
         }
        else{
@@ -88,7 +121,7 @@ const onPokedex =()=>{
             posicionActual = 0
             splash.style.animation = "none";
             errorMessage.textContent = '';
-            btnSearch.disabled = true;
+            // btnSearch.disabled = true;
             
         }
 })
@@ -96,47 +129,29 @@ const onPokedex =()=>{
 
 //imprime la imagen en el cuadro 
 const printData = (data)=>{
-    pictureBox.innerHTML = `<img src=${data.sprites.other.dream_world.front_default} alt="un pokemon">`;
+    pictureBox.innerHTML = `<img src=${data.sprites.other.dream_world.front_default} class="animada" alt="un pokemon">`;
     greenBoxUno.textContent = data.name.toUpperCase();
-    // glassIndicator.style.background = '-webkit-radial-gradient(var(--green-miniButton-glass-color-dos), var(--green-miniButton-glass-color-uno))' ;
-    crossActiva =true;
 }
 // NEXT POKEMON
 const nextPokemon = ()=>{
     
     rightCross.addEventListener('click',()=>{
-        // getTypePokemon(arrayAlmacenamiento[posicionActual+2].types[0].type.url);
-        // console.log(getTypePokemon(arrayAlmacenamiento[posicionActual+1].types[0].type.url));
         if(onOffButton.classList.contains('onoff') && crossActiva){
-            
             if((arrayAlmacenamiento.length-1) > posicionActual){
-                // esto funciona pero no esta bien. Me esta trayendo la promesa un ciclo mas tarde y no se porque.
-                getTypePokemon(arrayAlmacenamiento[posicionActual+2].types[0].type.url);
-            getTypeGenera(arraySpeciesPokemon[posicionActual+1]);
-            getDetails(arraySpeciesPokemon[posicionActual+1])
-                // getTypePokemon(arrayAlmacenamiento[posicionActual+2].types[0].type.url);
                 printData(arrayAlmacenamiento[posicionActual+1]);
-                // console.log(getTypePokemon(arrayAlmacenamiento[posicionActual+1].types[0].type.url));
-                speechName(generarTexto(arrayAlmacenamiento[posicionActual+1].name,typeEs,typeGeneraEs,TypeDetailEs));
+                speechName(generarTexto(arrayAlmacenamiento[posicionActual+1].name,getTypePokemonEs(typePokemonEn[posicionActual+1]),getTypeGenera(arraySpeciesPokemon[posicionActual+1]),getDetails(arraySpeciesPokemon[posicionActual+1])));
                 posicionActual++;
             }
-            
-       
-        }
-        
+        } 
     })
 }
 // BACK POKEMON
 const backPokemon = ()=>{
     leftCross.addEventListener('click',()=>{
         if(onOffButton.classList.contains('onoff') && crossActiva){
-           
             if(posicionActual> 0){
-                getTypePokemon(arrayAlmacenamiento[posicionActual-1].types[0].type.url);
-                getTypeGenera(arraySpeciesPokemon[posicionActual-1]);
-                getDetails(arraySpeciesPokemon[posicionActual-1])
                 printData(arrayAlmacenamiento[posicionActual-1]);
-                speechName(speechName(generarTexto(arrayAlmacenamiento[posicionActual-1].name,typeEs,typeGeneraEs,TypeDetailEs)));
+                speechName(generarTexto(arrayAlmacenamiento[posicionActual-1].name,getTypePokemonEs(typePokemonEn[posicionActual-1]),getTypeGenera(arraySpeciesPokemon[posicionActual-1]),getDetails(arraySpeciesPokemon[posicionActual-1])));
                 posicionActual--
             }
         }
@@ -149,49 +164,30 @@ const search = ()=>{
             btnSearch.addEventListener('click',()=>{
                 
                 pictureBox.innerHTML = `<img src="./assets/img/pokeball.png" alt="pokeball" class="pokeball" id="pokeball">`
+
                 errorMessage.innerHTML = '';
                 const textoIngresado = inputSearch.value.toLowerCase()
-                console.log(textoIngresado);
                 // el metodo find devuelve el elemento encontrado o sino devuelve undefined
                const pokemonExists =  arrayAlmacenamiento.find(e=>e.name === textoIngresado)
                const pokemonExistArraySpecies = arraySpeciesPokemon.find(e=>e.name === textoIngresado)
-              
-               
-                
-                    if(textoIngresado === ''){
-                        pictureBox.innerHTML = `<img src="./assets/img/caraTriste.png" alt="caraTriste" class="caraTriste" id="caraTriste">`
-                        errorMessage.textContent = 'Ingresa el nombre de un pokémon';
-                        console.log(errorMessage);
-                        console.log('vacio');
+               console.log(typePokemonEnPlusId);
+                if(textoIngresado === ''){
+                    pictureBox.innerHTML = `<img src="./assets/img/caraTriste.png"  alt="caraTriste" class="caraTriste" id="caraTriste">`
+                    errorMessage.textContent = 'Ingresa el nombre de un pokémon';
                        
-                    }
-                    else if(pokemonExists !== undefined & textoIngresado !== ''){
-                        console.log('dentro del else if');
-                        getTypePokemon(pokemonExists.types[0].type.url);
-                        console.log(pokemonExists.types[0].type.url);
-                        getTypeGenera(pokemonExistArraySpecies);
-                        console.log(pokemonExistArraySpecies);
-                        
-                        getDetails(pokemonExistArraySpecies)
-                        console.log(pokemonExistArraySpecies);
-                       
-                        setTimeout(()=>{
-                            printData(pokemonExists)
-                           
-                            speechName(generarTexto(pokemonExists.name,typeEs,typeGeneraEs,TypeDetailEs))
-                            // typeEs = '';
-                            // typeGeneraEs = '';
-                            // typeMoveEs = '';
-                            // TypeDetailEs = '';  
+                }
+                else if(pokemonExists !== undefined & textoIngresado !== ''){
+   
+                    setTimeout(()=>{
+                        printData(pokemonExists)
+                        speechName(generarTexto(pokemonExists.name,'sarasa',getTypeGenera(pokemonExistArraySpecies),getDetails(pokemonExistArraySpecies)))
                         },200,)
-                           
-            
-                        errorMessage.innerHTML = ''
+                        errorMessage.innerHTML = '';
+                        inputSearch.value = '';
                     }
-                    else{
-                        pictureBox.innerHTML = `<img src="./assets/img/caraTriste.png" alt="caraTriste" class="caraTriste" id="caraTriste">`
-                        errorMessage.innerHTML = `El pokémon ${textoIngresado} no existe`
-                        console.log('dentro del else');
+                else{
+                    pictureBox.innerHTML = `<img src="./assets/img/caraTriste.png" alt="caraTriste" class="caraTriste" id="caraTriste">`
+                    errorMessage.innerHTML = `El pokémon ${textoIngresado} no existe`   
                     }
                 
                 
@@ -203,24 +199,57 @@ const search = ()=>{
 
 //funcion para reproducir el nombre del pokemon por audio
 function speechName(text){
-    const cancelSpeech = ()=>{
-        speechSynthesis.cancel()
-    }
-    btnCancel.addEventListener('click', cancelSpeech)
     utterance.text = text ;
     utterance.rate = 5;
-    //esto es para que se pueda clickear el siguiente pokemon una vez que termine el audio
+    crossActiva = false;
+    btnSearch.disabled = true
+    //esto es para que se pueda clickear el siguiente pokemon una vez que termine el audio y no antes
     utterance.addEventListener('end',()=>{
         crossActiva = true;
         btnSearch.disabled = false
     })
-    crossActiva = false;
-    btnSearch.disabled = true
-    // btnSearch.setAttribute('disabled',true)
-    // utterance.voice = getVoice(2);
-    speechSynthesis.speak(utterance);
-    // const iMSpeaking = speechSynthesis.speaking
+    const crossClicked = ()=>{
+        if(speechSynthesis.paused){
+            crossActiva=true;
+            speechSynthesis.resume
+            speechSynthesis.cancel()
+        }
+    }
+    leftCross.addEventListener('click',()=>{
+        crossClicked();
+        backPokemon()
+    })
+    rightCross.addEventListener('click',()=>{
+        crossClicked();
+        nextPokemon()
+    })
     
+    speechSynthesis.speak(utterance);
+    // cancelar por completo el speech
+    const cancelSpeech = ()=>{
+        speechSynthesis.resume()
+        speechSynthesis.cancel()
+    }
+    btnCancel.addEventListener('click', cancelSpeech)
+
+    if(speechSynthesis.speaking){
+        onOffButton.addEventListener('click',cancelSpeech)
+        
+    }
+    // else{
+    //     crossActiva = true;
+    //     btnSearch.disabled = false
+    // }
+    // stop speech
+    const stopSpeech = ()=>{
+       if(speechSynthesis.speaking) speechSynthesis.pause();
+    }
+    btnStop.addEventListener('click',stopSpeech);
+    // // resume speech
+    const resumeSpeech = ()=>{
+        if(speechSynthesis.paused) speechSynthesis.resume()
+    }
+    btnResume.addEventListener('click',resumeSpeech)
 }
 // generar texto
 const generarTexto = (name,type,genera,details) =>{
@@ -228,24 +257,16 @@ const generarTexto = (name,type,genera,details) =>{
     return texto
 }
 
-
-    
-// optener el tipo de pokemon en espaniol porque por default viene en ingles
-const getTypePokemon = async(url) =>{
-    try {
-        const res = await fetch(url);
-        const  data = await res.json()
-        const typeSpanish = data.names.find(e=> e.language.name === 'es')
-        typeEs = typeSpanish.name
-    }catch (error) {
-        console.log(error);
-    }  
-    }
+// obteniendo el typo de pokemon en espaniol porque por defalt viene en ingles
+const getTypePokemonEs = (type)=>{
+    const typeSpanish = type.names.find(e => e.language.name === 'es');
+    return typeSpanish.name
+}
     // optener el genera del pokemon en espaniol porque por default viene en ingles
 const getTypeGenera = (pokemon)=>{ 
     const typeSpanish = pokemon.genera.find(e=> e.language.name === 'es')
    
-    typeGeneraEs = typeSpanish.genus
+    return typeSpanish.genus
 }
     
 // obtener los details de solo los primeros 3 details que sean en espaniol.
@@ -253,10 +274,13 @@ const getDetails = (pokemon)=>{
     
     const typeSpanish = pokemon.flavor_text_entries.filter(e=> e.language.name === 'es')
     const typeSpanishNoSpaces = typeSpanish.map(e=>e.flavor_text.replace(/\n/g," "))
-    TypeDetailEs = `${typeSpanishNoSpaces[0]} ${typeSpanishNoSpaces[1]} ${typeSpanishNoSpaces[2]}`
-    // console.log(`${typeSpanish[0].flavor_text} ${typeSpanish[1].flavor_text} ${typeSpanish[2].flavor_text}`);
+    return `${typeSpanishNoSpaces[0]} ${typeSpanishNoSpaces[1]} ${typeSpanishNoSpaces[2]}`
 }
+// console.log(arrayAlmacenamiento);
+
+
 //funcion que devuelve la voz seleccionada 0 y 1:mujer español, 2:hombre español
+
 // function getVoice(voiceIdx){
 //     const voices = window.speechSynthesis.getVoices();
 //     if(voiceIdx >= 0 && voiceIdx <voices.length)
